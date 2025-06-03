9c49bc236cdaa2cff6f240c55100740872cfed3e
Daniel Juape Abad <daniel.juape@gmail.com>
2025-06-03 21:25:52 +0200

    Initialize exam-solver-preprocessor with base Spring Boot config and dependencies

---
+++ exam-solver-preprocessor/src/main/java/com/examsolver/exam_solver_preprocessor/ExamSolverPreprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.examsolver.exam_solver_preprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSolverPreprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSolverPreprocessorApplication.class, args);
+	}
+
+}
---
+++ exam-solver-preprocessor/src/test/java/com/examsolver/exam_solver_preprocessor/ExamSolverPreprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsolver.exam_solver_preprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSolverPreprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
