885761895a4a647fb92d3501237458c31f1be628
Daniel Juape Abad <daniel.juape@gmail.com>
2025-06-03 22:04:41 +0200

    Add PreprocessController with mock response and request validation

---
+++ exam-solver-preprocessor/src/test/java/com/examsolver/preprocessor/ExamSolverPreprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsolver.preprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSolverPreprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
