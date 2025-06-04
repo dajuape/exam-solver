9f345d7bdeeed3e3c03a81f40f0ef1d33e1cfb11
Daniel Juape Abad <daniel.juape@gmail.com>
2025-06-04 20:16:35 +0200

    Update DTOs: add fileName, improve naming and validation annotations

--- exam-solver-shared/src/main/java/com/examsolver/shared/dtos/request/PreprocessRequestDTO.java
+++ exam-solver-shared/src/main/java/com/examsolver/shared/dtos/request/PreprocessRequestDTO.java
@@ -1,8 +1,8 @@
 package com.examsolver.shared.dtos.request;
 
 import com.examsolver.shared.enums.FileType;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +13,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PreprocessRequestDTO {
+
     @NotNull
     private FileType fileType;
+
     @NotNull
     private String base64File;
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String fileName;
 }
--- exam-solver-shared/src/main/java/com/examsolver/shared/dtos/response/PreprocessResponseDTO.java
+++ exam-solver-shared/src/main/java/com/examsolver/shared/dtos/response/PreprocessResponseDTO.java
@@ -1,13 +1,17 @@
 package com.examsolver.shared.dtos.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PreprocessResponseDTO {
     private boolean success;
-    private String text;
+    private String extractedText;
     private boolean fallbackRequired;
-    private String reason;
+    private String fallbackReason;
 }
