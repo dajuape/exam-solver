12867bf0c17e38e5db93fcc632f045e92f250a9f
Daniel Juape Abad <daniel.juape@gmail.com>
2025-06-24 22:30:19 +0200

    Change PreprocessResponseDTO adapt to return list of exercises

--- exam-solver-shared/src/main/java/com/examsolver/shared/dtos/response/PreprocessResponseDTO.java
+++ exam-solver-shared/src/main/java/com/examsolver/shared/dtos/response/PreprocessResponseDTO.java
@@ -5,13 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PreprocessResponseDTO {
     private boolean success;
-    private String extractedText;
+    private List<String> extractedText;
     private boolean fallbackRequired;
     private String fallbackReason;
 }
