82de74f2845e98e818a88985eb961c322d16e76b
Daniel Juape Abad <daniel.juape@gmail.com>
2025-08-03 19:38:36 +0200

    Initialize Spring Project

---
+++ exam-solver-openai/src/main/java/com/examsolver/openai/ExamSolverOpenaiApplication.java
@@ -0,0 +1,13 @@
+package com.examsolver.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSolverOpenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSolverOpenaiApplication.class, args);
+	}
+
+}
---
+++ exam-solver-openai/src/test/java/com/examsolver/openai/ExamSolverOpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examsolver.openai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSolverOpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
